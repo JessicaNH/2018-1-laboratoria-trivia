3bbc4fb59e78ad73f1d5c1d07b49a50cd03f26c0
Jessica Navarro <jess.nav359.herrera@gmail.com>
2018-05-18 11:51:48 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+
+     var jugar= document.getElementById("game").innerHTML
+   
+     if (jugar== "s") {
+        var answer1 = prompt("¿han egresado hombres en Laboratoria? s/n");
+        if (answer1.toLowerCase() == "n") {
+            document.getElementById("rightAnswersText").innerHTML =
+                document.getElementById("rightAnswersText").innerHTML +
+                "<div class='answer'>" +
+                "<p>No han egresado hombres</p>"
+            "</div>";
+        } else {
+            document.getElementById("wrongAnswersText").innerHTML =
+                document.getElementById("wrongAnswersText").innerHTML +
+                "<div class='answer'>" +
+                "<p>No han egresado hombres</p>"
+            "</div>";
+        }
+
+        var answer2 = prompt("¿hay laboratoria en concepción? s/n");
+        if (answer2.toLowerCase() == "n") {
+            document.getElementById("rightAnswersText").innerHTML =
+                document.getElementById("rightAnswersText").innerHTML +
+                "<div class='answer'>" +
+                "<p>No hay laboratoria en concepción</p>"
+            "</div>";
+        } else {
+            document.getElementById("wrongAnswersText").innerHTML =
+                document.getElementById("wrongAnswersText").innerHTML +
+                "<div class='answer'>" +
+                "<p>No hay laboratoria en concepción</p>"
+            "</div>";
+        }
+    } else {
+        document.getElementById("warningMessage").innerText = "Bueno Chao";
+    }
+}
